9ec37e25d3438eca6f711147f466c3dfe5971da4
Stuart <stupop010@hotmail.co.uk>
2020-03-20 14:40:49 +0000

    contact us

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
+import useForm from "../../hooks/useForm"
 
 import "./contactUsStyles.scss"
 
 const ContactUs = () => {
+  const { inputs, handleSubmit, handleInputChange } = useForm({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  })
+
   const { contactPic } = useStaticQuery(graphql`
     query {
       contactPic: file(relativePath: { eq: "contact-pic.jpeg" }) {
@@ -27,19 +35,50 @@ const ContactUs = () => {
         fluid={pic}
         backgroundColor={`#040e18`}
       >
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Contact</h2>
           <div className="title-underline"></div>
           <div className="input-container">
-            <div>
-              <input />
-              <input />
-              <input />
+            <div className="input-left">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                onChange={handleInputChange}
+                value={inputs.name}
+                className="form-input"
+              />
+              <input
+                placeholder="Email"
+                type="email"
+                name="email"
+                onChange={handleInputChange}
+                value={inputs.email}
+                className="form-input"
+              />
+              <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                onChange={handleInputChange}
+                value={inputs.subject}
+                className="form-input"
+              />
             </div>
-            <div>
-              <textarea />
+            <div className="input-left">
+              <textarea
+                type="text"
+                placeholder="Message"
+                name="message"
+                onChange={handleInputChange}
+                value={inputs.message}
+                className="form-input"
+              />
             </div>
           </div>
+          <button type="submit" className="form-btn">
+            Submit
+          </button>
         </form>
       </BackgroundImage>
     </section>
