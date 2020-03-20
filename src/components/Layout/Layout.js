ca92a39a10ea1bd1683441fd1e01e00f3a505245
Stuart <stupop010@hotmail.co.uk>
2020-03-20 15:10:42 +0000

    footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -21,10 +21,8 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer className="footer">
+          © {new Date().getFullYear()}, By Stuart Doney
         </footer>
       </div>
     </>
